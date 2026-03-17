c44e143e4d228506eb744fad1c1e08c66038b56f
SpencerTVaughan <spencer.vaughan@student.unimelb.edu.au>
2026-03-17 13:23:43 +1100

    Updated Vite Config for correct build paths and added sorucemap to avoid CSP issues in deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    base: "/ShinyLivingDexTracker/"
+  base: '/ShinyLivingDexTracker/',
+  build: {
+    sourcemap: false,
   },
 });
