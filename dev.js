f95114246b2f5c138ed1fde8032a850432a1493b
SpencerTVaughan <spencer.vaughan@student.unimelb.edu.au>
2026-03-17 20:15:49 +1100

    Updated dev.js for compatibility with Node v16

--- dev.js
+++ dev.js
@@ -1,3 +1,6 @@
 import { createJiti } from "jiti";
 const jiti = createJiti(import.meta.url);
-await jiti.import("./server.ts");
+jiti.import("./server.ts").catch((err) => {
+  console.error("Failed to start server:", err);
+  process.exit(1);
+});
